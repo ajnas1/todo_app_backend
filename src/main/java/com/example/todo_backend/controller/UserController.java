7f8a8d809671cd3374f8a67404194babf8a876f1
Ajnas <ajnasashkath@gmail.com>
2025-05-15 16:43:08 +0530

    minor change

--- src/main/java/com/example/todo_backend/controller/UserController.java
+++ src/main/java/com/example/todo_backend/controller/UserController.java
@@ -2,14 +2,12 @@ package com.example.todo_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.todo_backend.model.LoginModel;
 import com.example.todo_backend.model.UserModel;
-import com.example.todo_backend.service.JwtService;
 import com.example.todo_backend.service.UserInfoService;
 
 import jakarta.validation.Valid;
@@ -21,12 +19,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("auth")
 public class UserController {
+
     @Autowired
     private UserInfoService userInfoService;
-    @Autowired
-    private JwtService jwtService;
-    @Autowired
-    private AuthenticationManager authenticationManager;
+
     @GetMapping("welcome")
     public String welcome() {
         return "Welcome this endpoint is not secure";
