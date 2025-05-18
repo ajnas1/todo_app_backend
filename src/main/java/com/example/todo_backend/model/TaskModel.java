d4c23b59333724df5522482a301e8c0e11b58999
Ajnas <ajnasashkath@gmail.com>
2025-05-18 22:54:37 +0530

    user model and task model classes minor changes

--- src/main/java/com/example/todo_backend/model/TaskModel.java
+++ src/main/java/com/example/todo_backend/model/TaskModel.java
@@ -1,57 +1,42 @@
 package com.example.todo_backend.model;
 
-
 import java.util.Date;
-
-import com.example.todo_backend.utils.validation.PasswordConstraints;
-import com.example.todo_backend.utils.validation.PasswordMatches;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import jakarta.persistence.Transient;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "task")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@PasswordMatches
-@PasswordConstraints
-public class UserModel {
+public class TaskModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name ="id",unique = true,nullable = false )
-    private int id;
-    @Email(message = "Email should be valid")
-    private String email;
-    @NotEmpty(message = "Password should not be empty")
-    @NotBlank(message = "Password should not be null")
-    private String password;
-    @Transient
-    private String conformPassword;
-
+    private int taskId;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel user;
+    private String title;
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", updatable = false)
     private Date createdAt;
-
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at", updatable = true)
     private Date updatedAt;
-
     @PrePersist
     protected void onCreate() {
         createdAt = new Date();
